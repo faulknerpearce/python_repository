3decd3e5b9afe3eda2ec5b299c48a3a4f33cd256
James Bancroft <awesomeaustin1998@yahoo.com.au>
2023-02-10 20:24:37 -0700

    removed warnings on util

--- util.py
+++ util.py
@@ -1,3 +1,6 @@
 def freadlines(file_name):
-  f = open(file_name)
-  return f.readlines()
+    """
+    Given a file name it returns the file as a list
+    """
+    f = open(file_name)
+    return f.readlines()
