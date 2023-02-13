13481eb2238eb33b0458b4d9396b05a10b0479ac
James Bancroft <awesomeaustin1998@yahoo.com.au>
2023-02-13 13:47:58 -0700

    My pyhon sandbox

--- sandbox/dice.py
+++ sandbox/dice.py
@@ -6,8 +6,8 @@ while i < 2:
     num = random.randint(0, 10)
     dice_list.append(num)
     i += 1
-msg = " "
-
+#sum the roll of both dice
+dice_sum = 0
 for n in dice_list:
-    msg += str(n) + " "
-print(msg)
+    dice_sum += n 
+print(str(dice_sum))
