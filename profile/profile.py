3a724966ce48147d919d906374ff7a4f5c93d4c6
James Bancroft <awesomeaustin1998@yahoo.com.au>
2023-02-21 15:36:14 -0700

    profile.py (edit 3: added more funcionality)

--- profile/profile.py
+++ profile/profile.py
@@ -1,17 +1,67 @@
-profile = [["fireaustin", "7459", "Austin Faulkner-pearce", "25"], ["nicole", "ggg"]]
-index = 0
+profile = [["mr orange", "7459", "James Bancroft"], ["bob_007", "6677", "Bob Morewell"]]
 new = True
-user_name = input("Enter your username ")
-my_len = len(profile) -1
+user_in = 0
 
-while index < my_len:
-    if user_name != profile[index][0]:
-        index += 1
+while user_in != 2:
+    my_len = len(profile) - 1
+    print(profile)
+    user_name = input("\nEnter your username: ")
+    index = 0
     
-    elif user_name == profile[index][0]:
-        print("Welcome back " + str(profile[index][2]))
+    #This loop checks for an existing account with the username  
+    while index <= my_len:
+      if user_name == profile[index][0]:
         new = False
         break
+      else:
+        index += 1
+    
+    #Option to create an account 
+    if new == True:
+      print("\nWe could not find your account.")
+      user_in = input("Press(1) to create an account. Press(2) to quit: ")
+    else: 
+      #Password loop
+      tries = 3
+      print("\nAccount Located.")
+      while tries > 0: 
+          password = input("Enter your password: ")
+          if password != profile[index][1]:
+              tries += -1
+              print("incorrect password, try again.\nTries remaining: " + str(tries))
+              break
+              
+          else:
+              print("\n\nWelcome Back " + str(profile[index][2]))
+              user_in = 2
+              break #i can enter another loop here if i want to do something with the profile 
         
-if new == True:
-    print("We could not find your account")
+    #Account creation. 
+    if int(user_in) == 1:
+      #print("\nYou made it to the checkpoint")
+      i = 0
+      user_name_tries = 3
+      account_clash = False
+      
+      #Check if the username is taken 
+      while i < my_len and user_name_tries > 0:
+        new_user = input("Create a username: ")
+        if new_user == profile[i][0]:
+         user_name_tries += -1
+         print("This username is taken, Try again.\nTries remaining until timeout: " + str(user_name_tries)) 
+        else: 
+          i += 1
+      #append list if the username is available  
+      if account_clash == False:
+        print("\nThis username is available! ")
+        profile.append([new_user])
+        password = input("Create a password: ")
+        profile[-1].insert(1, password)
+        full_name = input("Enter your full name: ")
+        profile[-1].insert(2, full_name)
+        print(profile)
+    
+    if int(user_in) == 2:
+     print("\nProgram terminated.\n")
+     break
+    
