bc02ef23d2ec9284bb680e074a63c49b13673190
FaulknerPearce <awesomeaustin1998@yahoo.com.au>
2023-08-11 14:33:37 -0600

    added function descriptions

--- binary_search/func.py
+++ binary_search/func.py
@@ -1,22 +1,26 @@
- # This will calculate the median and decrease the max bound 
+ # Functions that will be used in the main script. 
+
+# Function to update the guess and bounds when the chosen number is below the current guess.
 def below(guess, bound):
     num = (guess - bound[0]) / 2  
-    bound[1] = guess
+    bound[1] = guess - 1
     guess -= num
     return round(guess)
       
-# This will calculate the median and increase the min bound
+# Function to update the guess and bounds when the chosen number is above the current guess.
 def above(guess, bound): 
     num = (bound[1] - guess) / 2
-    bound[0] = guess 
+    bound[0] = guess + 1
     guess += num 
     return round(guess)
-    
+
+# Function to ask the user if the guess is correct.
 def ask(guess):
-    output = int(input("Is your number: " + str(guess) + "? Press(1) For yes. Press(2) For No: "))
+    output = int(input("\n\nIs your number: " + str(guess) + "? Press(1) For yes. Press(2) For No: "))
     return output 
 
+# Function to ask the user if their number is below or above the current guess.
 def below_or_above(guess):
-    output = int(input("Press(1) if your number was below " + str(guess) + " Press(2) If it was above " + str(guess) +": "))
+    output = int(input("If your number was below " + str(guess) + " Press(1). If Your number was above " + str(guess) + " Press(2): "))
     return output
 
