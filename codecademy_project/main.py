40e2c14b86b56f35b19b3dc6eea2aa0d96e4f800
FaulknerPearce <awesomeaustin1998@yahoo.com.au>
2023-09-14 14:01:38 -0600

    updated scrpt

--- codecademy_project/main.py
+++ codecademy_project/main.py
@@ -1,96 +1,114 @@
 import random
 
-class Player:
+# The Class for creating the player and the enemies.
+class Character:
     # Create attributes for the player 
-    def __init__(self, name):
+    def __init__(self, name, health, weapon, race, level):
         self.name = name 
-        self.health = 100
-        self.level = 1
-        self.xp = 0
-        self.weapon = None
-    
-    def __repr__(self):
-        return f"This characters name is {self.name}, thier level is {self.level} and their curret health point are {self.health}"
-    
-    # Create methods for atacking, blocking, healing, and leveling up.
-    def attack(self, opponent):
-        damage = self.weapon.damage
-        if not opponent.defend:
-            opponent.health -= damage
-            print(f"Your strike was successful the {opponent.name} was dealt {damage} points of damage.")
-        else: 
-            print(f"The {opponent.name} blocked your attack. ")
-    
-    def defend(self, opponent):
-       # Calculate the success rate for defense based on skill level difference.
-       success_rate = min(1.0, max(0.0, 0.5 + (self.level - opponent.level) * 0.1))
-       
-       # This generates a boolean value based on whether a random number is less than the given success rate.
-       defence = random.random() < success_rate
-
-       if defence:
-           print(f"You successfully blocked the {opponent.name}'s attack.")
-       else:
-           print("You defence was unsuccsessful. ")
-       return defence 
-           
-class Goblin:
-    # Goblin attributes. 
-    def __init__(self, level):
-        self.name = "Goblin"
-        self.health = 90 + (level * 10)
+        self.health = health
+        self.weapon = weapon
+        self.race = race
         self.level = level
-        self.xp_value = 10 + (level * 5)
-
-    def __repr__(self):
-        return f"This is a level {self.level}, {self.name}."
     
-    def attack():
-        pass
-
-    def defend(self, opponent):
-       # Calculate the success rate for defense based on skill level difference.
-       success_rate = min(1.0, max(0.0, 0.5 + (self.level - opponent.level) * 0.1))
+    def __repr__(self):
+        if self.race == "Human":
+            return f"This characters name is {self.name}, thier level is {self.level} and their current health points are {self.health}."
+        else:
+            return f"This is a level {self.level} {self.name}."
+    
+    # This is a methond for the characters light attack. (Higher chance of succsessful hit.) 
+    def attack(self, opponent, the_style):
+            if the_style == 1:
+                damage = self.weapon.light_damage
+                opponent.health -= damage
+            else:
+                damage = self.weapon.heavy_damage
+                
+    # This is a method for the characters heavy attack. (Lesser chance of a succsessful hit.) 
+    def heavy_attack(self, opponent):
+        damage = self.weapon.heavy_damage
+    
+    def defend_block(self, opponent, attack_style):
        
+       attack_rate = {"light": 0.3, "heavy": 0.2}
+
+       adjusted_rate = attack_rate.get(attack_style, 0.5)
+    
+       # Calculate the success rate for defense based on skill level difference.
+       success_rate = min(1.0, max(0.0, 0.5 + (self.level - opponent.level) * 0.1 + adjusted_rate))
        # This generates a boolean value based on whether a random number is less than the given success rate.
        defence = random.random() < success_rate
+       
        return defence
 
-# This is a class for a sword.  
-class Sword:
-    # Sword attributes. 
-    def __init__(self):
-        self.level = 0
-        self.damage = 10 
-        self.xp = 0
+    def defend_parry():
+        pass 
 
+# This is a class for creating weapons.  
+class Weapon:
+    # Sword attributes. 
+    def __init__(self, light_damage, heavy_damage):
+        self.light_damage = light_damage
+        self.heavy_damage = heavy_damage
+
+    def parry(self):
+        pass
+      
 # This fucntion creates an instance of the character and sword. 
-def create_player_and_sword():
+def create_player():
     #name = input("Please enter you characters name: ")
-    created_player = Player("White Knight")
-    sword = Sword()
-    created_player.weapon = sword
+    sword = Weapon(10, 20)
+    created_player = Character("Galahad", 100, sword, "Human", 1)
     return created_player
 
 # This function will create an instance of a goblin that is the same level as the player. 
-def create_goblin(player_level):
-    created_goblin = Goblin(player_level)
+def create_goblin():
+    sword = Weapon(10, 20)
+    created_goblin = Character("Goblin", 100, sword, "Goblin", 1)
     return created_goblin
 
+def player_combat_attack(player, opponent):
+        
+        tactic = input("Press 1 for a Light Attack, Press 2 for a Heavy Attack.")
+        if int(tactic) == 1:
+            # opponents chance of succsessful block.
+            if opponent.defend_block(player, "light"):
+                player.attack(opponent, 1)
+                print(f"Your Light Attack was sucsessful. You delt {player.weapon.light_damage} points of damage to the {opponent.name}")
+            
+            else:
+                print(f"The {opponent.name} blocked your Light Attack.")
+
+
+        else: 
+            pass
+            # defending logic
+
+def player_combat_defend():
+    pass
+
+    
+
 # This function should check the level of the player after combat. 
-def combat(the_player, the_enemy):
+def arena(the_player, the_enemy):
     print(f"You are facing a level {the_enemy.level} {the_enemy.name} in the arena.")
 
-    choice = input(f"You can start with an attack on the {the_enemy.name}, or you can prepare to block. \nPress 1 to attack, Press 2 to block: ")
+    while the_player.health > 0 and the_enemy.health > 0:
 
-    if int(choice) == 1:
-        the_enemy.defend(the_player)
-        the_player.attack(the_enemy)
 
-# Create a function for combat between the player and the enemy. 
-player = create_player_and_sword()
+        player_combat_attack(the_player, the_enemy)
+        print(f"health of goblin: {the_enemy.health}")
 
-goblin = create_goblin(player.level)
+        #player_combat_defend(the_player, the_enemy)
+    
 
-combat(player, goblin)
+
+
+player = create_player()
+
+goblin = create_goblin()
+
+arena(player, goblin)
+
+
 
