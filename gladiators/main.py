c0f0efb9792f78bd559d2f814c3b9e2c50923904
FaulknerPearce <awesomeaustin1998@yahoo.com.au>
2023-09-15 14:31:49 -0600

    created an attacking function and a int_to_word function for calculating light of heavy attack damage.

--- gladiators/main.py
+++ gladiators/main.py
@@ -19,27 +19,29 @@ class Character:
     
     # This is a the characters methond for attacking.
     def attack(self, opponent, attack_style):
-            if attack_style:
-                # Light attack damage. 
-                damage = self.weapon.light_damage
-                opponent.health -= damage
-            else:
-                # Heavy attack damage.
-                damage = self.weapon.heavy_damage
-                opponent.health -= damage
+        if attack_style == "Light":
+        # Light attack damage. 
+            damage = self.weapon.light_damage
+            opponent.health -= damage
+        else:
+            # Heavy attack damage.
+            damage = self.weapon.heavy_damage
+            opponent.health -= damage
+        return damage
     
     # This method is for the characters defence.          
-    def defend_block(self, opponent, attack_key):
-        
-       attack_rate = {"light": 0.3, "heavy": 0.2}
-       # adjusted attack rate for light and heavy attack.
-       adjusted_rate = attack_rate.get(attack_key, 0.5)
+    def block(self, opponent, attack_key):
+        # adjusted attack rate for light and heavy attack.
+        attack_rate = {"Light": 0.5, "Heavy": 0.3}
+        adjusted_rate = attack_rate.get(attack_key, 0.3)
     
-       # Calculate the success rate for defense based on skill level difference and attack style.
-       success_rate = min(1.0, max(0.0, 0.5 + (self.level - opponent.level) * 0.1 + adjusted_rate))
-       # This generates a boolean value based on whether a random number is less than the given success rate.
-       defence = random.random() < success_rate
-       return defence
+        # Calculate the success rate for defense based on skill level difference and attack style.
+        success_rate = min(1.0, max(0.0, 0.3 + (self.level - opponent.level) * 0.1 + adjusted_rate)) 
+        print(f"seccsess rate: {success_rate}")
+        
+        # If the random flaot is greater than the success rate this will return True.
+        defence = round(random.random(),1) > success_rate
+        return defence
     
     def defend_parry():
         pass
@@ -51,10 +53,6 @@ class Weapon:
         self.light_damage = light_damage
         self.heavy_damage = heavy_damage
     
-    # create a method for parrying against an attack.  
-    def parry(self):
-        pass
-      
 # This fucntion creates an instance of the character and sword. 
 def create_player():
     #name = input("Please enter you characters name: ")
@@ -68,40 +66,37 @@ def create_goblin():
     created_goblin = Character("Goblin", 100, sword, "Goblin", 1)
     return created_goblin
 
-def player_combat_attack(player, opponent):
-        
-        tactic = input("Press 1 for a Light Attack, Press 2 for a Heavy Attack.")
-        if int(tactic) == 1:
-            # opponents chance of succsessful block.
-            if opponent.defend_block(player, "light"):
-                player.attack(opponent, True)
-                print(f"Your Light Attack was sucsessful. You delt {player.weapon.light_damage} points of damage to the {opponent.name}")
-            else:
-                print(f"The {opponent.name} blocked your Light Attack.")  
-        else: 
-            pass
-            # defending logic
+# This fucntion will use a int as a key for a dictionary and retrun the associated word. 
+def convert_int_to_word(player_choice):
+    attacks = {1: "Light", 2: "Heavy"}
+    return attacks.get(int(player_choice), 3)
 
-def player_combat_defend():
+def attacking_turn(player, opponent, tactic):
+    # If the opponents block is False, then reduce opponents health by the calculated damage of the attack (Light or Heavy). 
+    if not opponent.block(player, tactic):
+        damage_points = player.attack(opponent, tactic)
+        print(f"Your {tactic} Attack was succsessful, you delt {damage_points} points of damage.\nOpponent remaining health points: {opponent.health}")
+    
+    # Otherwise no damage is dealt to the opponents and the players attacking turn is over. 
+    else:
+        print(f"The {opponent.name} blocked your {tactic} Attack.")
+
+def defending_turn():
     pass
 
-# This function should check the level of the player after combat. 
-def arena(the_player, the_enemy):
-    print(f"You are facing a level {the_enemy.level} {the_enemy.name} in the arena.")
+# Begining of the main game program. 
+print(f"Welcome to the Arena, you have been selected for a fight to the death. \n\nPlease chose you character name.")
 
-    while the_player.health > 0 and the_enemy.health > 0:
-
-
-        player_combat_attack(the_player, the_enemy)
-        print(f"health of goblin: {the_enemy.health}")
-
-        #player_combat_defend(the_player, the_enemy)
-    
 player = create_player()
 
 goblin = create_goblin()
 
-arena(player, goblin)
+print(f"You are facing a level {goblin.level} {goblin.race}.")
+while player.health > 0 and goblin.health > 0:
 
+    # Add attack probability to this statement. 
+    choice = input(f"Press 1 for a Light Attack, Press 2 for a Heavy Attack. ")
+    attack = convert_int_to_word(int(choice))
+    print(f"The attack word: {attack}")
 
-
+    attacking_turn(player, goblin, attack)
