b616eba66a85995e591799f248a359c620df1151
FaulknerPearce <awesomeaustin1998@yahoo.com.au>
2023-11-20 17:16:36 -0700

    refactor

--- algorithms/proof_of_work/proof_of_work.py
+++ algorithms/proof_of_work/proof_of_work.py
@@ -4,7 +4,7 @@ from hashlib import sha256
 new_transactions = [{'amount': '30', 'sender':'alice', 'receiver':'bob'},{'amount': '55', 'sender':'bob', 'receiver':'alice'}]
 
 # This function will find a hash that has the required amount of leading zeros.
-def pow(transactions, difficulty):
+def proof_of_work(transactions, difficulty):
     nonce = 0
     proof = sha256((str(nonce) + str(transactions)).encode()).hexdigest()
     while proof[:difficulty] != "0" * difficulty: 
@@ -12,7 +12,7 @@ def pow(transactions, difficulty):
         proof = sha256((str(nonce)+str(transactions)).encode()).hexdigest()
     return proof 
 
-final_proof = pow(new_transactions, 3)
+final_proof = proof_of_work(new_transactions, 3)
 
 # Output the result that was found. 
 print(f"final proof: {final_proof}")
