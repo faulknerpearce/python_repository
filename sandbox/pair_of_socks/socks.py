77b1ec9f2efcf586d610488027f33c2fe92ccfca
FaulknerPearce <austinfaulknerpearce@yahoo.com>
2024-05-28 14:25:07 -0600

    refactored script

--- sandbox/pair_of_socks/socks.py
+++ sandbox/pair_of_socks/socks.py
@@ -1,34 +1,27 @@
 import random
 
-# Creates a list of socks with specified colors.
-def create_socks(pairs):
-    socks = []
-   
-    for colour in range(pairs):
-        for _ in range(10):
-            socks.append(colour)
-    
-    return socks
+# Generates a list containing ten socks for each specified color.
+def create_socks_list(colours):
+    return [i for i in range(colours) for _ in range(10)]
 
 # Simulates finding a pair of socks in a shuffled list.
-def find_pair(socks):
+def find_pair(socks_draw):
     collected_socks = {}
-    turns = 0
+    tries = 0 
 
-    for sock in socks:
-        turns += 1
+    for sock in socks_draw:
+        tries += 1
 
-        if collected_socks.get(sock, 0):
+        if collected_socks.get(sock):
             collected_socks[sock] += 1
-            if collected_socks[sock] == 2:
-                return turns
+            break
         else:
             collected_socks.update({sock: 1})
-    
-    return turns
+
+    return tries 
 
 # Simulates finding pairs multiple times and records the number of turns.
-def generate_tires(socks, amount_of_tries):
+def generate_tires_arr(socks, amount_of_tries):
     result = []
 
     for _ in range(amount_of_tries):
@@ -46,12 +39,10 @@ def calculate_average(arr):
     return round(average)
 
 # ________Main Program_________ #
-if __name__ == '__main__':
+my_socks = create_socks_list(2)
 
-    my_socks = create_socks(2)
+my_tires = generate_tires_arr(my_socks, 1000)
 
-    my_tires = generate_tires(my_socks, 1000)
+the_average = calculate_average(my_tires)
 
-    the_average = calculate_average(my_tires)
-
-    print(f'The average number of atempts needed to get a pair of socks is: {the_average}')
+print(f'The average number of atempts needed to get a pair of socks is: {the_average}')
