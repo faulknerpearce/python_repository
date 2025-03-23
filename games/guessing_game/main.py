09fd378ddefce5b061e067ac99207f02c6a1c065
FaulknerPearce <austinfaulknerpearce@yahoo.com>
2025-03-23 07:19:57 -0600

    created the main script for the game

--- games/guessing_game/main.py
+++ games/guessing_game/main.py
@@ -1,51 +1,33 @@
- # This will calculate the median and decrease the max bound 
-def below(guess, bound):
-    num = (guess - bound[0]) / 2  
-    bound[1] = guess
-    guess -= num
-    return round(guess)
-      
-# This will calculate the median and increase the min bound
-def above(guess, bound): 
-    num = (bound[1] - guess) / 2
-    bound[0] = guess 
-    guess += num 
-    return round(guess)
-    
-def ask(guess):
-    output = int(input("Is your number: " + str(guess) + "? Press(1) For yes. Press(2) For No: "))
-    return output 
+from game_functions import below, above, ask, below_or_above
 
-def below_or_above(guess):
-    output = int(input("Press(1) if your number was below " + str(guess) + " Press(2) If it was above " + str(guess) +": "))
-    return output
+if __name__ == "__main__":
 
-# Main 
-bounds = [0, 100]
-guess = 50
-tries = 0
-response = 2
+    bounds = [0, 100]
+    guess = 50
+    tries = 0
+    response = 2
 
-number = int(input("Please chose a number between 0 and 100 "))
+    print('For this game you will pick a number and i will try to guess it in less than ten tries.')
+    number = int(input("Please chose a number between 0 and 100 "))
 
-# The last task to complete: check if the guess is equal to the chosen number 
-while tries < 11 and response == 2:
+    # The last task to complete: check if the guess is equal to the chosen number 
+    while tries < 11 and response == 2:
 
-    response = ask(guess)
+        response = ask(guess)
 
-    if response == 2:
-        pinpoint = below_or_above(guess)
-    
-    if pinpoint == 1:
-        guess = below(guess, bounds)
+        if response == 2:
+            pinpoint = below_or_above(guess)
+        
+        if pinpoint == 1:
+            guess = below(guess, bounds)
 
-    elif pinpoint == 2:
-        guess = above(guess, bounds)
+        elif pinpoint == 2:
+            guess = above(guess, bounds)
 
-    tries += 1 
+        tries += 1 
 
-if tries < 11:
-    print("I guessed your number! I win. ")
+    if tries < 11:
+        print("I guessed your number! I win. ")
 
-else: 
-    print("I was unable to guess your number, You win. ")
+    else: 
+        print("I was unable to guess your number, You win. ")
