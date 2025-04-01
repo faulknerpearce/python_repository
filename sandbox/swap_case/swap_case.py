096d7ccf42c6e01e60fcfed51a31ee1b1439a5b7
FaulknerPearce <austinfaulknerpearce@yahoo.com>
2025-04-01 05:47:31 -0600

    created a script for swapping cases

--- sandbox/swap_case/swap_case.py
+++ sandbox/swap_case/swap_case.py
@@ -1,18 +1,25 @@
-my_sentence = 'HackerRank.com presents "Pythonist 2".'
-
+# Swaps the case of each letter in the given string: uppercase letters become lowercase, lowercase letters become uppercase.
 def swap_case(s):
     result = ''
 
     for char in s:
         character = ord(char)
-        
-        # Check if the character is uppercase.
+    
         if character >= 65 and character <= 90:
             character += 32
-        # Check if the character is lowercase.
+   
         elif character >= 97 and character <= 122:
             character -= 32
         
         converted = chr(character)
         result += converted
+    
     return result
+
+if __name__ == '__main__':
+
+    my_sentence = 'HackerRank.com presents "Pythonist 2".'
+
+    swapped_sentence = swap_case(my_sentence)
+
+    print(swapped_sentence)
