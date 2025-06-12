a7e05b589bfcfdeb086cf7703d214d45322b2e51
FaulknerPearce <austinfaulknerpearce@yahoo.com>
2025-06-12 08:15:19 +0800

    added commets to the encrypt file

--- sandbox/encrypt/encrypt.py
+++ sandbox/encrypt/encrypt.py
@@ -1,5 +1,6 @@
 import random
 
+# Shifts a letter by a given amount for encryption or decryption
 def shift_letter(letter, amount, encrypt):
     if encrypt:
         return (ord(letter) + amount)
@@ -7,6 +8,7 @@ def shift_letter(letter, amount, encrypt):
         return (ord(letter) - amount)
 
 
+# Encrypts or decrypts a single letter using a shift amount
 def encrypt_decrypt(letter, amount, encrypt):
 
     shifted = shift_letter(letter, amount, encrypt)
@@ -15,6 +17,7 @@ def encrypt_decrypt(letter, amount, encrypt):
     return chr(result + 97)
 
 
+# Encrypts a string, returning the encrypted string and the list of shift numbers
 def encrypt_string(string):
     result = ''
     numbers = []
@@ -27,6 +30,7 @@ def encrypt_string(string):
         
     return result, numbers
 
+# Decrypts a string using the provided list of shift numbers
 def decrypt_string(string, numbers):
     result = ''
 
@@ -44,7 +48,3 @@ if __name__ == '__main__':
     name = decrypt_string('jvsjyd', [9, 1, 26, 16, 16, 16])
 
     print(name)
-
-
-
-
